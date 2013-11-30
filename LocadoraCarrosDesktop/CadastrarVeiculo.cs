d8c3e4e79c909c916a665f9709c116bef33abaf5
gapessoa <gabriel@controlnet.com.br>
2013-11-29 23:29:48 -0200

    Cadastrar Veiculo Mockup

---
+++ LocadoraCarrosDesktop/CadastrarVeiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraCarrosDesktop
+{
+    public partial class CadastrarVeiculo : Form
+    {
+        public CadastrarVeiculo()
+        {
+            InitializeComponent();
+        }
+    }
+}
