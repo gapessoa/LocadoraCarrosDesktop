f9758ad6e9efb3dfbe2ba83c9e08adaa9b278e3c
gapessoa <gabriel@controlnet.com.br>
2013-11-30 00:36:03 -0200

    CadastrarCondutor

---
+++ LocadoraCarrosDesktop/CadastrarCondutor.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace LocadoraCarrosDesktop
+{
+    partial class CadastrarCondutor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtKm = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCor = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtChassi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.locadoraDataSet = new LocadoraCarrosDesktop.locadoraDataSet();
+            this.locadoraDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.locadoraDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locadoraDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(55, 54);
+            this.txtNome.MaxLength = 255;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(183, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtKm);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtAno);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtModelo);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtCor);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtChassi);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPlaca);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 346);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastrar Condutor";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 48);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 243);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Combustível";
+            // 
+            // txtKm
+            // 
+            this.txtKm.Location = new System.Drawing.Point(55, 210);
+            this.txtKm.MaxLength = 10;
+            this.txtKm.Name = "txtKm";
+            this.txtKm.Size = new System.Drawing.Size(183, 20);
+            this.txtKm.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Km";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(55, 184);
+            this.txtAno.MaxLength = 10;
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(183, 20);
+            this.txtAno.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ano";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(55, 158);
+            this.txtModelo.MaxLength = 100;
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(183, 20);
+            this.txtModelo.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "CNH";
+            // 
+            // txtCor
+            // 
+            this.txtCor.Location = new System.Drawing.Point(55, 132);
+            this.txtCor.MaxLength = 100;
+            this.txtCor.Name = "txtCor";
+            this.txtCor.Size = new System.Drawing.Size(183, 20);
+            this.txtCor.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "E-mail";
+            // 
+            // txtChassi
+            // 
+            this.txtChassi.Location = new System.Drawing.Point(55, 106);
+            this.txtChassi.MaxLength = 17;
+            this.txtChassi.Name = "txtChassi";
+            this.txtChassi.Size = new System.Drawing.Size(183, 20);
+            this.txtChassi.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "RG";
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(55, 80);
+            this.txtPlaca.MaxLength = 8;
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(183, 20);
+            this.txtPlaca.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPF";
+            // 
+            // locadoraDataSet
+            // 
+            this.locadoraDataSet.DataSetName = "locadoraDataSet";
+            this.locadoraDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // locadoraDataSetBindingSource
+            // 
+            this.locadoraDataSetBindingSource.DataSource = this.locadoraDataSet;
+            this.locadoraDataSetBindingSource.Position = 0;
+            // 
+            // CadastrarCondutor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 361);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CadastrarCondutor";
+            this.Text = "Cadastrar Condutor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.locadoraDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locadoraDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtKm;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtChassi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Label label2;
+        private locadoraDataSet locadoraDataSet;
+        private System.Windows.Forms.BindingSource locadoraDataSetBindingSource;
+    }
+}
