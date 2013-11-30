d58c250067ea8822779d3f7c4f919185ac3f1b91
gapessoa <gabriel@controlnet.com.br>
2013-11-30 00:36:14 -0200

    Cadastrar Veiculo

--- LocadoraCarrosDesktop/CadastrarVeiculo.cs
+++ LocadoraCarrosDesktop/CadastrarVeiculo.cs
@@ -15,6 +15,56 @@ namespace LocadoraCarrosDesktop
         public CadastrarVeiculo()
         {
             InitializeComponent();
+
+            Dictionary<string, string> cb_data_marcas = new Dictionary<string, string>();
+            Dictionary<string, string> cb_data_combustiveis = new Dictionary<string, string>();
+
+            theConn conn = new theConn();
+            theConn conn2 = new theConn();
+
+            var marcas = conn.Select("SELECT id,nome FROM veiculo_marcas");
+
+            foreach (var marca in marcas)
+            {
+                cb_data_marcas.Add(marca["id"].ToString(), marca["nome"].ToString());
+            }
+
+            cbMarca.DataSource = new BindingSource(cb_data_marcas, null);
+            cbMarca.DisplayMember = "Value";
+            cbMarca.ValueMember = "Key";
+
+
+            var combustiveis = conn2.Select("SELECT id,nome FROM combustivel");
+
+            foreach (var combustivel in combustiveis)
+            {
+                cb_data_combustiveis.Add(combustivel["id"].ToString(), combustivel["nome"].ToString());
+            }
+
+            cbCombustivel.DataSource = new BindingSource(cb_data_combustiveis, null);
+            cbCombustivel.DisplayMember = "Value";
+            cbCombustivel.ValueMember = "Key";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            theConn conn = new theConn();
+
+            conn.InsertItem("nome", txtNome.Text);
+            conn.InsertItem("placa", txtPlaca.Text);
+            conn.InsertItem("chassi", txtChassi.Text);
+            conn.InsertItem("cor", txtCor.Text);
+            conn.InsertItem("modelo", txtModelo.Text);
+            conn.InsertItem("ano", txtAno.Text);
+            conn.InsertItem("km", txtKm.Text);
+            conn.InsertItem("tipo_combustivel", cbCombustivel.SelectedValue.ToString());
+            conn.InsertItem("marca_id", cbMarca.SelectedValue.ToString());
+
+            conn.Insert("veiculos");
+
+            MessageBox.Show("O veículo foi cadastrado com sucesso!");
+
+            this.Close();
         }
     }
 }
