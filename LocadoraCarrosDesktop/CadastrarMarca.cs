c8dc6152defed2ce20b8e2fedf056599236cc5d7
gapessoa <gabriel@controlnet.com.br>
2013-11-29 23:28:46 -0200

    CadastrarMarca Working

---
+++ LocadoraCarrosDesktop/CadastrarMarca.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraCarrosDesktop
+{
+    public partial class CadastrarMarca : Form
+    {
+        public CadastrarMarca()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            theConn conn = new theConn();
+
+            conn.InsertItem("nome", textBox1.Text);
+            conn.Insert("veiculo_marcas");
+
+            MessageBox.Show("Sua marca foi cadastrada com sucesso!");
+
+            this.Close();
+        }
+    }
+}
