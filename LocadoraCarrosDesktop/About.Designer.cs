a7b6fecbd6ecde5b5dd7204ce26383505d75f3e4
gapessoa <gabriel@controlnet.com.br>
2013-11-29 23:29:17 -0200

    About Working

---
+++ LocadoraCarrosDesktop/About.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace LocadoraCarrosDesktop
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labVersion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdAction = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.labVersion);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 172);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.LinkArea = new System.Windows.Forms.LinkArea(0, 36);
+            this.linkLabel1.Location = new System.Drawing.Point(0, 136);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(228, 33);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/gapessoa";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.linkLabel1.UseCompatibleTextRendering = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(8, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "29 de Novembro de 2013";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(8, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "por Gabriel Pessoa";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labVersion
+            // 
+            this.labVersion.Location = new System.Drawing.Point(8, 44);
+            this.labVersion.Name = "labVersion";
+            this.labVersion.Size = new System.Drawing.Size(224, 16);
+            this.labVersion.TabIndex = 1;
+            this.labVersion.Text = "Um programa feito em C#";
+            this.labVersion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CarLoc V0.0.1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cmdAction
+            // 
+            this.cmdAction.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmdAction.Location = new System.Drawing.Point(86, 200);
+            this.cmdAction.Name = "cmdAction";
+            this.cmdAction.Size = new System.Drawing.Size(75, 23);
+            this.cmdAction.TabIndex = 5;
+            this.cmdAction.Text = "OK";
+            this.cmdAction.Click += new System.EventHandler(this.cmdAction_Click);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 238);
+            this.Controls.Add(this.cmdAction);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "About";
+            this.Text = "About";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labVersion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdAction;
+
+    }
+}
---
+++ LocadoraCarrosDesktop/About.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraCarrosDesktop
+{
+    public partial class About : Form
+    {
+
+        Form1 form1;
+
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/gapessoa/LocadoraCarrosDesktop");
+        }
+
+        private void cmdAction_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
