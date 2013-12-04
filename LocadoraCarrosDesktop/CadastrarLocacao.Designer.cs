f9c9a631392fbffe53142a72c4512825cfd6bbbe
Gabriel Pessoa <gabriel@controlnet.com.br>
2013-12-04 21:55:05 -0200

    Img View Add

--- LocadoraCarrosDesktop/CadastrarLocacao.Designer.cs
+++ LocadoraCarrosDesktop/CadastrarLocacao.Designer.cs
@@ -29,32 +29,34 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtKmSaida = new System.Windows.Forms.MaskedTextBox();
+            this.txtDiariasPrevistas = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDataSaida = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFranquiaDia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.cbTipo = new System.Windows.Forms.ComboBox();
             this.Tipo = new System.Windows.Forms.Label();
             this.txtDataReserva = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtFranquiaDia = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtDestino = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtDataSaida = new System.Windows.Forms.MaskedTextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtDiariasPrevistas = new System.Windows.Forms.MaskedTextBox();
-            this.txtKmSaida = new System.Windows.Forms.MaskedTextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.txtDataContrato = new System.Windows.Forms.MaskedTextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.cbVeiculo = new System.Windows.Forms.ComboBox();
-            this.cbCondutor = new System.Windows.Forms.ComboBox();
-            this.label9 = new System.Windows.Forms.Label();
             this.cbLocatario = new System.Windows.Forms.ComboBox();
             this.label10 = new System.Windows.Forms.Label();
+            this.cbCondutor = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cbVeiculo = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDataContrato = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.pctCarro = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCarro)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -79,6 +81,90 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dados Locação";
+            // 
+            // txtKmSaida
+            // 
+            this.txtKmSaida.Location = new System.Drawing.Point(284, 162);
+            this.txtKmSaida.Mask = "000000";
+            this.txtKmSaida.Name = "txtKmSaida";
+            this.txtKmSaida.Size = new System.Drawing.Size(121, 20);
+            this.txtKmSaida.TabIndex = 14;
+            // 
+            // txtDiariasPrevistas
+            // 
+            this.txtDiariasPrevistas.Location = new System.Drawing.Point(284, 78);
+            this.txtDiariasPrevistas.Mask = "00";
+            this.txtDiariasPrevistas.Name = "txtDiariasPrevistas";
+            this.txtDiariasPrevistas.Size = new System.Drawing.Size(22, 20);
+            this.txtDiariasPrevistas.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(192, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "KM Saída";
+            // 
+            // txtDataSaida
+            // 
+            this.txtDataSaida.Location = new System.Drawing.Point(86, 162);
+            this.txtDataSaida.Mask = "00/00/0000";
+            this.txtDataSaida.Name = "txtDataSaida";
+            this.txtDataSaida.Size = new System.Drawing.Size(100, 20);
+            this.txtDataSaida.TabIndex = 11;
+            this.txtDataSaida.ValidatingType = typeof(System.DateTime);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Data Saída";
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(86, 116);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(319, 20);
+            this.txtDestino.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Destino";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Diárias Previstas";
+            // 
+            // txtFranquiaDia
+            // 
+            this.txtFranquiaDia.Location = new System.Drawing.Point(86, 75);
+            this.txtFranquiaDia.Name = "txtFranquiaDia";
+            this.txtFranquiaDia.Size = new System.Drawing.Size(100, 20);
+            this.txtFranquiaDia.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Franquia Dia";
             // 
             // cbTipo
             // 
@@ -116,90 +202,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Data Reserva";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Franquia Dia";
-            // 
-            // txtFranquiaDia
-            // 
-            this.txtFranquiaDia.Location = new System.Drawing.Point(86, 75);
-            this.txtFranquiaDia.Name = "txtFranquiaDia";
-            this.txtFranquiaDia.Size = new System.Drawing.Size(100, 20);
-            this.txtFranquiaDia.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(192, 78);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Diárias Previstas";
-            // 
-            // txtDestino
-            // 
-            this.txtDestino.Location = new System.Drawing.Point(86, 116);
-            this.txtDestino.Name = "txtDestino";
-            this.txtDestino.Size = new System.Drawing.Size(319, 20);
-            this.txtDestino.TabIndex = 9;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 119);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Destino";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 165);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(62, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Data Saída";
-            // 
-            // txtDataSaida
-            // 
-            this.txtDataSaida.Location = new System.Drawing.Point(86, 162);
-            this.txtDataSaida.Mask = "00/00/0000";
-            this.txtDataSaida.Name = "txtDataSaida";
-            this.txtDataSaida.Size = new System.Drawing.Size(100, 20);
-            this.txtDataSaida.TabIndex = 11;
-            this.txtDataSaida.ValidatingType = typeof(System.DateTime);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(192, 165);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(55, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "KM Saída";
-            // 
-            // txtDiariasPrevistas
-            // 
-            this.txtDiariasPrevistas.Location = new System.Drawing.Point(284, 78);
-            this.txtDiariasPrevistas.Mask = "00";
-            this.txtDiariasPrevistas.Name = "txtDiariasPrevistas";
-            this.txtDiariasPrevistas.Size = new System.Drawing.Size(22, 20);
-            this.txtDiariasPrevistas.TabIndex = 13;
-            // 
-            // txtKmSaida
-            // 
-            this.txtKmSaida.Location = new System.Drawing.Point(284, 162);
-            this.txtKmSaida.Mask = "000000";
-            this.txtKmSaida.Name = "txtKmSaida";
-            this.txtKmSaida.Size = new System.Drawing.Size(121, 20);
-            this.txtKmSaida.TabIndex = 14;
-            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.cbLocatario);
@@ -216,60 +218,6 @@
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Dados Contrato";
-            // 
-            // txtDataContrato
-            // 
-            this.txtDataContrato.Location = new System.Drawing.Point(85, 39);
-            this.txtDataContrato.Mask = "00/00/0000";
-            this.txtDataContrato.Name = "txtDataContrato";
-            this.txtDataContrato.Size = new System.Drawing.Size(100, 20);
-            this.txtDataContrato.TabIndex = 16;
-            this.txtDataContrato.ValidatingType = typeof(System.DateTime);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 39);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(30, 13);
-            this.label7.TabIndex = 15;
-            this.label7.Text = "Data";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(9, 83);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 13);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "Veículo";
-            // 
-            // cbVeiculo
-            // 
-            this.cbVeiculo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbVeiculo.FormattingEnabled = true;
-            this.cbVeiculo.Location = new System.Drawing.Point(85, 80);
-            this.cbVeiculo.Name = "cbVeiculo";
-            this.cbVeiculo.Size = new System.Drawing.Size(319, 21);
-            this.cbVeiculo.TabIndex = 15;
-            // 
-            // cbCondutor
-            // 
-            this.cbCondutor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbCondutor.FormattingEnabled = true;
-            this.cbCondutor.Location = new System.Drawing.Point(85, 107);
-            this.cbCondutor.Name = "cbCondutor";
-            this.cbCondutor.Size = new System.Drawing.Size(319, 21);
-            this.cbCondutor.TabIndex = 18;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(9, 110);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(50, 13);
-            this.label9.TabIndex = 19;
-            this.label9.Text = "Condutor";
             // 
             // cbLocatario
             // 
@@ -289,6 +237,61 @@
             this.label10.TabIndex = 21;
             this.label10.Text = "Locatário";
             // 
+            // cbCondutor
+            // 
+            this.cbCondutor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCondutor.FormattingEnabled = true;
+            this.cbCondutor.Location = new System.Drawing.Point(85, 107);
+            this.cbCondutor.Name = "cbCondutor";
+            this.cbCondutor.Size = new System.Drawing.Size(319, 21);
+            this.cbCondutor.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Condutor";
+            // 
+            // cbVeiculo
+            // 
+            this.cbVeiculo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbVeiculo.FormattingEnabled = true;
+            this.cbVeiculo.Location = new System.Drawing.Point(85, 80);
+            this.cbVeiculo.Name = "cbVeiculo";
+            this.cbVeiculo.Size = new System.Drawing.Size(319, 21);
+            this.cbVeiculo.TabIndex = 15;
+            this.cbVeiculo.SelectedIndexChanged += new System.EventHandler(this.cbVeiculo_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Veículo";
+            // 
+            // txtDataContrato
+            // 
+            this.txtDataContrato.Location = new System.Drawing.Point(85, 39);
+            this.txtDataContrato.Mask = "00/00/0000";
+            this.txtDataContrato.Name = "txtDataContrato";
+            this.txtDataContrato.Size = new System.Drawing.Size(100, 20);
+            this.txtDataContrato.TabIndex = 16;
+            this.txtDataContrato.ValidatingType = typeof(System.DateTime);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Data";
+            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(176, 418);
@@ -299,11 +302,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pctCarro
+            // 
+            this.pctCarro.Location = new System.Drawing.Point(499, 90);
+            this.pctCarro.Name = "pctCarro";
+            this.pctCarro.Size = new System.Drawing.Size(217, 248);
+            this.pctCarro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pctCarro.TabIndex = 4;
+            this.pctCarro.TabStop = false;
+            // 
             // CadastrarLocacao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 484);
+            this.ClientSize = new System.Drawing.Size(753, 479);
+            this.Controls.Add(this.pctCarro);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -313,6 +326,7 @@
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCarro)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -344,5 +358,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.MaskedTextBox txtDataContrato;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pctCarro;
     }
 }
--- LocadoraCarrosDesktop/CadastrarLocacao.cs
+++ LocadoraCarrosDesktop/CadastrarLocacao.cs
@@ -12,28 +12,28 @@ namespace LocadoraCarrosDesktop
 {
     public partial class CadastrarLocacao : Form
     {
+        private string privatePath = @"C:\Users\Gabriel\Documents\GitHub\LocadoraCarrosDesktop\LocadoraCarrosDesktop\Img\";
+
         public CadastrarLocacao()
         {
             InitializeComponent();
 
+            this.load_Veiculos();
             this.load_CbTipo();
             this.load_Condutores();
-            this.load_Veiculos();
             this.load_Locatarios();
-
-            cbTipo.DropDownStyle = ComboBoxStyle.DropDownList;
 
         }
 
         private void load_CbTipo()
         {
-             Dictionary<string, string> tipoLocacao = new Dictionary<string, string>();
-             tipoLocacao.Add("0", "Diária");
-             tipoLocacao.Add("1", "Mensal");
+            Dictionary<string, string> tipoLocacao = new Dictionary<string, string>();
+            tipoLocacao.Add("0", "Diária");
+            tipoLocacao.Add("1", "Mensal");
 
-             cbTipo.DataSource = new BindingSource(tipoLocacao, null);
-             cbTipo.DisplayMember = "Value";
-             cbTipo.ValueMember = "Key";
+            cbTipo.DataSource = new BindingSource(tipoLocacao, null);
+            cbTipo.DisplayMember = "Value";
+            cbTipo.ValueMember = "Key";
         }
 
         private void load_Condutores()
@@ -54,6 +54,8 @@ namespace LocadoraCarrosDesktop
 
         private void load_Veiculos()
         {
+            cbVeiculo.SelectedIndexChanged -= new System.EventHandler(this.cbVeiculo_SelectedIndexChanged);
+
             theConn conn = new theConn();
             var rows = conn.Select("SELECT id,nome FROM veiculos WHERE alugado = 0 ORDER BY nome ASC");
             Dictionary<string, string> cbData = new Dictionary<string, string>();
@@ -66,6 +68,13 @@ namespace LocadoraCarrosDesktop
             cbVeiculo.DataSource = new BindingSource(cbData, null);
             cbVeiculo.DisplayMember = "Value";
             cbVeiculo.ValueMember = "Key";
+
+            cbVeiculo.SelectedIndexChanged += new System.EventHandler(this.cbVeiculo_SelectedIndexChanged);
+
+            cbVeiculo.SelectedIndex = 1;
+            cbVeiculo.SelectedIndex = 0;
+
+
         }
 
         private void load_Locatarios()
@@ -88,14 +97,14 @@ namespace LocadoraCarrosDesktop
         {
             theConn conn = new theConn();
 
-            conn.InsertItem("data_reserva",  Convert.ToDateTime(txtDataReserva.Text).ToString("yyyy-MM-dd"));
+            conn.InsertItem("data_reserva", Convert.ToDateTime(txtDataReserva.Text).ToString("yyyy-MM-dd"));
             conn.InsertItem("tipo_locacao", cbTipo.SelectedValue.ToString());
             conn.InsertItem("franquia_dia", txtFranquiaDia.Text);
             conn.InsertItem("diarias_previstas", txtDiariasPrevistas.Text);
             conn.InsertItem("destino", txtDestino.Text);
-            conn.InsertItem("data_saida",  Convert.ToDateTime(txtDataSaida.Text).ToString("yyyy-MM-dd"));
+            conn.InsertItem("data_saida", Convert.ToDateTime(txtDataSaida.Text).ToString("yyyy-MM-dd"));
             conn.InsertItem("km_saida", txtKmSaida.Text);
-            conn.InsertItem("data_contrato",  Convert.ToDateTime(txtDataContrato.Text).ToString("yyyy-MM-dd"));
+            conn.InsertItem("data_contrato", Convert.ToDateTime(txtDataContrato.Text).ToString("yyyy-MM-dd"));
             conn.InsertItem("veiculo_id", cbVeiculo.SelectedValue.ToString());
             conn.InsertItem("condutor_id", cbCondutor.SelectedValue.ToString());
             conn.InsertItem("locatario_id", cbLocatario.SelectedValue.ToString());
@@ -109,6 +118,25 @@ namespace LocadoraCarrosDesktop
             MessageBox.Show("Locação cadastrada com sucesso!");
 
             this.Close();
+        }
+
+        private void cbVeiculo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string veiculoID = cbVeiculo.SelectedValue.ToString();
+
+            Console.WriteLine(veiculoID);
+            theConn conn3 = new theConn();
+
+            var veiculoPhoto = conn3.Select("SELECT imgfile FROM veiculos WHERE id = '" + veiculoID + "' ORDER BY nome ASC");
+
+            string file = veiculoPhoto[0]["imgfile"].ToString();
+
+            string filename = file + ".png";
+
+            pctCarro.Image = Image.FromFile(this.privatePath + filename);
+
+
         }
     }
 }
